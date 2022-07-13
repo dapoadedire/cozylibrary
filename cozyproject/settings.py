2fd74df95a3b7f83bd29c5098070974817b16ae9
dapoadedire <adedireadedapo19@gmail.com>
2022-07-13 17:51:29 +0100

    feat: configure reset_password mail settings

--- cozyproject/settings.py
+++ cozyproject/settings.py
@@ -157,14 +157,33 @@ LOGOUT_REDIRECT_URL = "book:home"
 
 
 
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # TODO: Give APP Password here
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool)
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_HOST = config('EMAIL_HOST')
-# EMAIL_PORT = config('EMAIL_PORT')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # TODO: Give APP Password here
-# EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-# EMAIL_TIMEOUT = config('EMAIL_TIMEOUT', default=10, cast=int)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
