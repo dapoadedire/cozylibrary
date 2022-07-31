a3ce2b10eb31c40fd1e08abedce43220aa852946
dapoadedire <adedireadedapo19@gmail.com>
2022-07-31 17:30:17 +0100

    ready to deploy

--- cozyproject/settings.py
+++ cozyproject/settings.py
@@ -161,7 +161,7 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER' , default = '-')
 EMAIL_HOST = config('EMAIL_HOST' , default = '-')
 EMAIL_PORT = config('EMAIL_PORT' , default = '-')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD' , default = '-')  # TODO: Give APP Password here
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default = '-', cast=bool)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default = True, cast=bool)
 
 
 
